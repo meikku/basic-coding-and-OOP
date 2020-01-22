6ac3556de4e3aecf159c14c4dc823cf260ed6920
Merja Pyrhönen <meikkupyrhonen@Merja-MacBook-Air.local>
2020-01-22 23:09:21 +0200

    exercises 21-22 done exercise 23 - something wrong!?

--- part1/calculations/exercise_22/Program.cs
+++ part1/calculations/exercise_22/Program.cs
@@ -6,6 +6,22 @@ namespace exercise_22
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      string userFirst = Console.ReadLine();
+      int first = Convert.ToInt32(userFirst);
+
+      Console.WriteLine("Give the second number!");
+      string userSecond = Console.ReadLine();
+      int second = Convert.ToInt32(userSecond);
+
+      Console.WriteLine("Give the third number!");
+      string userThird = Console.ReadLine();
+      int third = Convert.ToInt32(userThird);
+
+      double average = (first + second + third) / 3.0;
+
+      Console.WriteLine("The average is " + average);
+
 
       // Write your code here:
 
--- part1/calculations/exercise_23/Program.cs
+++ part1/calculations/exercise_23/Program.cs
@@ -6,6 +6,25 @@ namespace exercise_23
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      string userFirst = Console.ReadLine();
+      int first = Convert.ToInt32(userFirst); 
+
+      Console.WriteLine("Give the second number!");
+      string userSecond = Console.ReadLine();
+      int second = Convert.ToInt32(userSecond); 
+
+      int sum = first + second;
+      int subtraction = first - second; 
+      int multiplication = first * second;
+      double division = 1.0 * first / second;
+
+      Console.WriteLine(first + " + " + second + " = " + sum);
+      Console.WriteLine(first + " - " + second + " = " + subtraction);
+      Console.WriteLine(first + " * " + second + " = " + multiplication);
+      Console.WriteLine(first + " / " + second + " = " + division);
+
+
 
       // Write your code here:
 
