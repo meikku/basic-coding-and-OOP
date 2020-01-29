bd0cdb54973f1052a232abf3f0b94f36c94c578d
Merja Pyrhönen <meikkupyrhonen@Merja-MacBook-Air.local>
2020-01-29 22:42:50 +0200

    exercises 52-53 done

--- part2/methods/exercise_52/Program.cs
+++ part2/methods/exercise_52/Program.cs
@@ -7,9 +7,16 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      PrintFromNumberToOne(5);
     }
-
+    public static void PrintFromNumberToOne(int number)
+    {
+      while(number >= 1)
+      {
+        Console.WriteLine(number);
+        number = number - 1;
+      }
+    }
     // Write your method here:
 
   }
