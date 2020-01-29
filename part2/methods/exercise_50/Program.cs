3a88922a972a967b6bf3ba14f7cb557418afa2e9
Merja Pyrhönen <meikkupyrhonen@Merja-MacBook-Air.local>
2020-01-29 10:24:51 +0200

    49 fail, 50 done, 51: work in progress...

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,11 +7,25 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
+      Console.WriteLine("How many times?");
+      string userInput = Console.ReadLine();
+      int number = Convert.ToInt32(userInput); 
+
+      int amount = 0; 
+
+      while (amount < number)
+      {
+        PrintPhrase();
+        amount = amount + 1;
+      }
+      
+
  
     }
 
     // Write your method here:
     public static void PrintPhrase() {
+      Console.WriteLine("In a hole in the ground there lived a method");
 
     }
   }
--- part2/methods/exercise_51/Program.cs
+++ part2/methods/exercise_51/Program.cs
@@ -7,11 +7,23 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      PrintUntilNumber();
 
     }
 
     // Write your method here:
+    public static void PrintUntilNumber(int number)
+    {
+      string userInput = Console.ReadLine();
+      int number = Convert.ToInt32(userInput);
+      int amount = 0;
+      while (amount < number) 
+      {
+        Console.WriteLine(amount);
+        amount = amount + 1;
+      }
+ 
+    }
 
   }
 }
