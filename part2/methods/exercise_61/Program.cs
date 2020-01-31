0af7f61fb57f5c75152023879c72fdce402fc7d9
Merja Pyrhönen <meikkupyrhonen@Merja-MacBook-Air.local>
2020-01-31 16:23:19 +0200

    61 done - merry xmas!

--- part2/methods/exercise_61/Program.cs
+++ part2/methods/exercise_61/Program.cs
@@ -6,27 +6,65 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+      
+    
+      PrintRightTriangle(10);
+      ChristmasTree(12);
+      
+    
     }
 
-    public static void PrintStars(int v)
+    public static void PrintStars(int number)
     {
-
+      int round = 1; 
+      while(round<=number)
+      { Console.Write("*");
+        round++;
+      }
+      Console.WriteLine("");
     }
 
-    public static void PrintSpaces(int v)
+    public static void PrintSpaces(int number)
     {
-
+      int round = 1; 
+      while (round<=number)
+      { Console.Write(" ");
+        round++;
+      }
     }
 
-    public static void PrintRightTriangle(int v)
+    public static void PrintRightTriangle(int size)
     {
-
+      int round = 1;
+      int stars = 1; 
+      while (round <= size)
+      {
+        PrintSpaces(size - round);
+        PrintStars(stars);
+        stars++;
+        round++;
+      }
+      
     }
 
-    public static void ChristmasTree(int v)
+    public static void ChristmasTree(int height)
     {
+      int round = 1;
+      int width = 1;
+      int spaces = (height - 1);
 
+      while (round <= height)
+      {
+        PrintSpaces(spaces);
+        PrintStars(width);
+        width = width + 2;
+        round++;
+        spaces--;
+      }
+        PrintSpaces(height - 2);
+        PrintStars(3);
+        PrintSpaces(height -2); 
+        PrintStars(3);
     }
   }
 }
