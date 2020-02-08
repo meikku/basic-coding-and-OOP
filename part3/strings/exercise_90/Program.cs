ffc2d901ff5351d6878855ba56f3aa2f80efb266
meikku <59999917+meikku@users.noreply.github.com>
2020-02-08 08:46:47 +0200

    90 done

--- part3/strings/exercise_90/Program.cs
+++ part3/strings/exercise_90/Program.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 
 namespace exercise_90
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            int oldest = 0;          
+            while (true) // tehdään luuppi
+            {
+                string input = Console.ReadLine(); // luetaan syöte käyttäjältä
+                if (input == "")
+                {
+                    break; // jos syöte on tyhjä, lopetetaan
+                }
 
-
+                string[] pieces = input.Split(","); // jaetaan syöte pilkun kohdalta
+                int age = Convert.ToInt32(pieces[1]); // muunnetaan syötteen jälkiosa luvuksi
+                
+                if (age > oldest) {
+                  oldest = age;
+                }
+                
+            }
+            Console.WriteLine("Age of the oldest: " + oldest);
+        }
     }
-  }
 }
 
 
