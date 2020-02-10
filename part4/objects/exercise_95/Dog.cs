d3970d962d80c0494bf8acf68c6bd2b94c01dc11
meikku <59999917+meikku@users.noreply.github.com>
2020-02-10 22:05:13 +0200

    ex. 95-97 done

---
+++ part4/objects/exercise_95/Dog.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace exercise_95
+{
+ public class Dog   
+ {
+    private string name;
+    private string breed;
+
+    private int age;
+
+ }
+}
---
+++ part4/objects/exercise_96/Room.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace exercise_96
+{
+    public class Room
+    {
+        private string code;
+        private int seats;
+
+        public Room(string classCode, int numberOfSeats)
+        {
+            this.code = classCode;
+            this.seats = 0;
+        }
+
+    }
+}
