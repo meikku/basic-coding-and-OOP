59e4cdd832b2fc5af5ca2d5d2ee31b826fd24aca
meikku <59999917+meikku@users.noreply.github.com>
2020-02-12 22:10:57 +0200

    ex 98 done

---
+++ part4/objects/exercise_98/Product.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace exercise_98
+{
+    public class Product
+    {
+        private double price;
+        private int quantity;
+        private string name;
+
+        public Product(string name, double price, int quantity)
+        {
+
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public void PrintProduct() {
+            Console.WriteLine(this.name + ": price " + this.price + ": " + this.quantity + " pcs");
+        }
+
+
+    }
+
+}
