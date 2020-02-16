c14f271a109324597808cd80a4c181390b884700
meikku <59999917+meikku@users.noreply.github.com>
2020-02-16 11:58:32 +0200

    ex. 112 done

--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -8,8 +8,29 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+      List <TelevisionProgram> tvprograms = new List<TelevisionProgram>();
+      while (true)
+      {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
+        Console.Write("Duration: ");
+        int duration = Convert.ToInt32(Console.ReadLine());
 
+        tvprograms.Add(new TelevisionProgram(name, duration));
+      }
+      Console.WriteLine("");
+      Console.Write("Program's maximum duration? ");
+      int maxDuration = Convert.ToInt32(Console.ReadLine());
 
+      foreach (TelevisionProgram program in tvprograms)
+      { if (maxDuration >= program.duration) {
+        Console.WriteLine(program);
+      }
+      }
     }
   }
 }
