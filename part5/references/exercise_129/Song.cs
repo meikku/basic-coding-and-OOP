7737f7242ac4c3f85938a8d78fbd68db057a7f54
meikku <59999917+meikku@users.noreply.github.com>
2020-02-23 15:12:38 +0200

    ex. 129 test 6/ 8 pass

--- part5/references/exercise_129/Song.cs
+++ part5/references/exercise_129/Song.cs
@@ -1,33 +1,38 @@
 namespace exercise_129
 {
 
-  public class Song
-  {
-
-    private string artist;
-    private string name;
-    private int durationInSeconds;
-
-    public Song(string artist, string name, int durationInSeconds)
+    public class Song
     {
-      this.artist = artist;
-      this.name = name;
-      this.durationInSeconds = durationInSeconds;
-    }
 
-    public override string ToString()
-    {
-      return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
-    }
+        private string artist;
+        private string name;
+        private int durationInSeconds;
 
-    // BEGIN SOLUTION
-    public override bool Equals(object compared)
-    {
- 
-      return false;
-    }
-    // END SOLUTION
+        public Song(string artist, string name, int durationInSeconds)
+        {
+            this.artist = artist;
+            this.name = name;
+            this.durationInSeconds = durationInSeconds;
+        }
 
-  }
+        public override string ToString()
+        {
+            return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+        }
+
+        // BEGIN SOLUTION
+        public override bool Equals(object compared)
+        {
+        
+            if (this.artist.Equals(compared) && (this.name.Equals(compared)) && this.durationInSeconds.Equals(compared))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // END SOLUTION
+
+    }
 
 }
