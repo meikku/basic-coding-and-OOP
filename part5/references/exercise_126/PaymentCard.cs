155cf898122fc3ab94fd93a3bce18194e66e0ac6
meikku <59999917+meikku@users.noreply.github.com>
2020-02-23 10:39:10 +0200

    ex 126 done

--- part5/references/exercise_126/PaymentCard.cs
+++ part5/references/exercise_126/PaymentCard.cs
@@ -2,7 +2,8 @@ namespace exercise_126
 {
   public class PaymentCard
   {
-    public double balance { get; private set; }
+    public double balance { get; set; }
+    
 
     public PaymentCard(double balance)
     {
@@ -16,6 +17,11 @@ namespace exercise_126
 
     public bool TakeMoney(double amount)
     {
+      if (this.balance >= amount)
+      {
+        this.balance = this.balance - amount;
+        return true;
+      }
       // implement the method so that it only takes money from the card if
       // the balance is at least the amount parameter.
       // returns true if successful and false otherwise
