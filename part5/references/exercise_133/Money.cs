0e7cb664d39d50d58cf653598cbf6377059e251f
meikku <59999917+meikku@users.noreply.github.com>
2020-02-25 11:07:54 +0200

    ex 133 done - with a little help from my friends...

--- part5/references/exercise_133/Money.cs
+++ part5/references/exercise_133/Money.cs
@@ -1,56 +1,105 @@
 namespace exercise_133
 {
-  public class Money
-  {
-
-    private int euros;
-    private int cents;
-
-    public Money(int euros, int cents)
+    public class Money
     {
-      if (cents > 99)
-      {
-        euros = euros + cents / 100;
-        cents = cents % 100;
-      }
 
-      this.euros = euros;
-      this.cents = cents;
+        private int euros;
+        private int cents;
+
+        public Money(int euros, int cents)
+        {
+            if (cents > 99)
+            {
+                euros = euros + cents / 100;
+                cents = cents % 100;
+            }
+            
+
+            this.euros = euros;
+            this.cents = cents;
+        }
+
+        public Money Plus(Money addition)
+        {
+            Money newMoney = new Money(this.euros, this.cents);
+            // create a new Money object that has the correct worth
+            newMoney.euros = this.euros + addition.euros;
+            newMoney.cents = this.cents + addition.cents;
+
+            if (newMoney.cents > 99)
+            {
+                newMoney.euros = newMoney.euros + newMoney.cents / 100;
+                newMoney.cents = newMoney.cents % 100;
+            }
+            return newMoney;
+            // return the new Money object
+           
+        }
+
+        public Money Minus(Money decreaser)
+        {
+            Money newMoney = new Money(this.euros, this.cents);
+            // create a new Money object that has the correct worth
+            
+           newMoney.euros = this.euros - decreaser.euros;
+           newMoney.cents = this.cents - decreaser.cents;
+
+            if (newMoney.cents < 0)
+            {  
+              newMoney.euros = newMoney.euros -1;
+              newMoney.cents = newMoney.cents + 100;
+            }
+
+            if (newMoney.euros < 0)
+            {
+              newMoney.euros = 0;
+              newMoney.cents = 0;
+              return newMoney;
+            }
+            if (newMoney.euros >= 0)
+            {
+              return newMoney;
+            }
+            else if (newMoney.cents < 0)
+            {
+              newMoney.euros = 0;
+              newMoney.cents = 0;
+              return newMoney;
+            }
+            // return the new Money object
+          
+            return newMoney;
+        }
+
+
+        public bool LessThan(Money compared)
+        {
+            // Do something here
+            if (this.euros < compared.euros)
+            {
+              return true;
+            }
+            if (this.euros > compared.euros)
+            {
+              return false;
+            }
+            else if (this.cents < compared.cents)
+            {
+              return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string zero = "";
+            if (cents < 10)
+            {
+                zero = "0";
+            }
+
+            return euros + "." + zero + cents + "e";
+        }
     }
-
-    public Money Plus(Money addition)
-    {
-      Money newMoney = new Money(/* Do something here*/);
-      // create a new Money object that has the correct worth
-
-      // return the new Money object
-      return newMoney;
-    }
-
-    public Money Minus(Money decreaser)
-    {
-      Money newMoney = new Money(/* Do something here*/);
-      // create a new Money object that has the correct worth
-
-      // return the new Money object
-      return newMoney;
-    }
-
-    public bool LessThan(Money compared)
-    {
-      // Do something here
-      return false;
-    }
-
-    public override string ToString()
-    {
-      string zero = "";
-      if (cents < 10)
-      {
-        zero = "0";
-      }
-
-      return euros + "." + zero + cents + "e";
-    }
-  }
 }
--- part5/references/exercise_133/Program.cs
+++ part5/references/exercise_133/Program.cs
@@ -7,28 +7,28 @@ namespace exercise_133
   {
     public static void Main(string[] args)
     {
-      Money money = new Money(100, 00);
+      Money money = new Money(100, 00);   
       Money moreMoney = new Money(500, 50);
 
       Money combined = money.Plus(moreMoney);
 
-      Console.WriteLine(money);
-      Console.WriteLine(moreMoney);
-      Console.WriteLine(combined);
+      Console.WriteLine(money); // 100.00
+      Console.WriteLine(moreMoney); // 500.50
+      Console.WriteLine(combined); // 600.50
 
       Money lessMoney = moreMoney.Minus(money);
 
-      Console.WriteLine(money);
-      Console.WriteLine(moreMoney);
-      Console.WriteLine(lessMoney);
+      Console.WriteLine(money); // 100.00
+      Console.WriteLine(moreMoney); // 500.50
+      Console.WriteLine(lessMoney); // 400.50
 
       lessMoney = lessMoney.Minus(money);
 
-      Console.WriteLine(money);
-      Console.WriteLine(moreMoney);
-      Console.WriteLine(lessMoney);
+      Console.WriteLine(money); // 100.00
+      Console.WriteLine(moreMoney); // 500.50
+      Console.WriteLine(lessMoney); // 300.50
 
-      Console.WriteLine(lessMoney.LessThan(moreMoney));
+      Console.WriteLine(lessMoney.LessThan(moreMoney)); 
       Console.WriteLine(lessMoney.LessThan(money));
 
       lessMoney = lessMoney.Minus(moreMoney);
