528483771b1acb188078f8b223e3165353b2c6eb
meikku <59999917+meikku@users.noreply.github.com>
2020-03-10 21:11:07 +0200

    extras 4-5 done

--- extra/extra_04/Program.cs
+++ extra/extra_04/Program.cs
@@ -7,6 +7,40 @@ namespace extra_04
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("Give your percent [0 - 100]:");
+      int percent = Convert.ToInt32(Console.ReadLine());
+      if (percent > 100)
+      {
+        Console.WriteLine("Outstanding!");
+      }
+      else if (percent >= 90)
+      {
+        Console.WriteLine("Grade: 5");
+      }
+      else if (percent >= 80)
+      {
+        Console.WriteLine("Grade: 4");
+      }
+      else if (percent >= 70)
+      {
+        Console.WriteLine("Grade: 3");
+      }
+      else if (percent >= 60)
+      {
+        Console.WriteLine("Grade: 2");
+      }
+      else if (percent >= 50)
+      {
+        Console.WriteLine("Grade: 1");
+      }
+      else if (percent >= 0)
+      {
+        Console.WriteLine("Fail");
+      }
+      else if (percent < 0)
+      {
+        Console.WriteLine("Impossible");
+      }
 
     }
   }
--- extra/extra_05/Program.cs
+++ extra/extra_05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_05
 {
@@ -7,7 +8,27 @@ namespace extra_05
     public static void Main(string[] args)
     {
       // Add your code here:
+      for (int i = 1; i <= 5; i++)
+      {
+        Console.WriteLine(i);
+      }
 
+      List<string> list = new List<string>();
+      Console.WriteLine("Give words, 'end' quits:");
+
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "end")
+        {
+          break;
+        }
+        list.Add(input);
+      }
+      foreach (string line in list)
+      {
+        Console.WriteLine(line);
+      }
     }
   }
 }
