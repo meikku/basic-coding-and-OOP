302aae7acfa72eeb91afd281932cb44043699d42
meikku <59999917+meikku@users.noreply.github.com>
2020-03-11 20:06:54 +0200

    extra 12 done

---
+++ extra/extra_12/Person.cs
@@ -0,0 +1,25 @@
+namespace extra_12
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name)
+        {
+            this.name = name;
+            this.age = 0;
+        }
+        public int GrowOlder(int howMuch)
+        {
+            this.age = this.age + howMuch;
+            return this.age;
+        }
+        public override string ToString()
+        {
+            return this.name + ", age " + this.age;
+            
+        }
+    }
+}
+
--- extra/extra_12/Program.cs
+++ extra/extra_12/Program.cs
@@ -2,12 +2,15 @@
 
 namespace extra_12
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
-
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            Person mike = new Person("Mike");
+            Console.WriteLine(mike);
+            mike.GrowOlder(5);
+            Console.WriteLine(mike);
+        }
     }
-  }
 }
