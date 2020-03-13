72921781ffe9e36c2f1d8b49858e588da5c0d756
meikku <59999917+meikku@users.noreply.github.com>
2020-03-13 08:45:16 +0200

    ex. 15 done

--- extra/extra_15/Program.cs
+++ extra/extra_15/Program.cs
@@ -2,12 +2,14 @@
 
 namespace extra_15
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
+        public static void Main(string[] args)
+        {
 
+            // Add your code here:
+            Cube cube = new Cube(3);
+            Console.WriteLine(cube);
+        }
     }
-  }
 }
