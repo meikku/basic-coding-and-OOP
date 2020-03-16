c773be88fa97d0ed48e0e4a114d9c3ece2a3c37f
meikku <59999917+meikku@users.noreply.github.com>
2020-03-16 15:49:15 +0200

    ex 74 fixed

--- part3/lists/exercise_74/Program.cs
+++ part3/lists/exercise_74/Program.cs
@@ -1,28 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_74 {
-  class Program {
-    public static void Main (string[] args) {
-      List<string> list = new List<string> ();
-      while (true) {
-        string input = Console.ReadLine ();
-        if (input == " ") {
-          break;
-        }
-        list.Add (input);
-      }
-      Console.WriteLine ("Search for?");
-      string searchable = Console.ReadLine ();
+namespace exercise_74
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            Console.WriteLine("Search for?");
+            string searchable = Console.ReadLine();
 
-      while (true) {
-        if (list.Contains (searchable)) {
-          Console.WriteLine (searchable + " was found!");
-        } else {
-          Console.WriteLine (searchable + " was not found!");
-        }
-      }
 
+            if (list.Contains(searchable))
+            {
+                Console.WriteLine(searchable + " was found!");
+            }
+            else
+            {
+                Console.WriteLine(searchable + " was not found!");
+            }
+
+
+        }
     }
-  }
 }
