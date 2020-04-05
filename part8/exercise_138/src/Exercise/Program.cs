0120cf5c9d8959f35803b56b2fb12e31904329a0
meikku <59999917+meikku@users.noreply.github.com>
2020-04-05 19:59:18 +0300

    NewTypes2 structure finished

--- part8/exercise_138/src/Exercise/Program.cs
+++ part8/exercise_138/src/Exercise/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      // Do something here
+        public static void Main(string[] args)
+        {
+            // Do something here
+            Dictionary<string, string> names = new Dictionary<string, string>();
+            names.Add("matthew", "matt");
+            names.Add("michael", "mix");
+            names.Add("arthur", "artie");
 
-      }
+            foreach (KeyValuePair<string, string> kvp in names)
+            {
+                Console.WriteLine(kvp.Key + "'s nickname is " + kvp.Value);
+            }
+        }
     }
-  }
 }
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -8,31 +8,14 @@ namespace sandbox
         static void Main(string[] args)
         {
 
-             public void Advance(int howManyDays)
-        {
-            int daysLimit = 30;
-
-            day += howManyDays;
-            if (howManyDays > daysLimit)
-            {
-              int months = howManyDays / daysLimit;
-              month = month + months;
-              int remainder = howManyDays - (months * daysLimit);
-            }
-            if (day > daysLimit)
-            {
-              day = day - daysLimit;
-              month++;
-            }
-
-            if (month == 13)
-            {
-              month = 1;
-              year++;
-            }
-
-
         }
+
+
+
+
+
+
     }
 }
+
 
