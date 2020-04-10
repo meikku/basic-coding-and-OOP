b06fc91b57941d1c59a3036b4c8f0bc0fba2bda8
meikku <59999917+meikku@users.noreply.github.com>
2020-04-10 12:44:48 +0300

    ex 141 done

--- part8/exercise_141/src/Exercise/Program.cs
+++ part8/exercise_141/src/Exercise/Program.cs
@@ -1,31 +1,45 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace Exercise
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      Dictionary<string, Book> books = new Dictionary<string, Book>();
-      Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
-      Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "....");
-      books.Add(senseAndSensibility.name, senseAndSensibility);
-      books.Add(prideAndPrejudice.name, prideAndPrejudice);
+        public static void Main(string[] args)
+        {
+            Dictionary<string, Book> books = new Dictionary<string, Book>();
+            Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
+            Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "....");
+            books.Add(senseAndSensibility.name, senseAndSensibility);
+            books.Add(prideAndPrejudice.name, prideAndPrejudice);
 
-      PrintValues(books);
-      Console.WriteLine("-- -- -- --");
-      PrintValueIfNameContains(books, "prejud");
+            PrintValues(books);
+            Console.WriteLine("-- -- -- --");
+            PrintValueIfNameContains(books, "prejud");
+        }
+
+
+        public static void PrintValues(Dictionary<string, Book> dictionary)
+        {
+            foreach (KeyValuePair<string, Book> book in dictionary)
+            {
+                Console.WriteLine(book.Value);
+            }
+        }
+        public static void PrintValueIfNameContains(Dictionary<string, Book> dictionary, string text)
+        {
+            foreach (KeyValuePair<string, Book> book in dictionary)
+            {
+                string name = book.Value.name.ToLower();
+                if (name.Contains(text.ToLower()))
+                    
+                    {
+                        Console.WriteLine(book.Value);
+                    }
+
+            }
+        }
     }
-
-    public static void PrintValues(Dictionary<string, Book> dictionary)
-    {
-
-    }
-    public static void PrintValueIfNameContains(Dictionary<string, Book> dictionary, string text)
-    {
-
-    }
-  }
 }
 
