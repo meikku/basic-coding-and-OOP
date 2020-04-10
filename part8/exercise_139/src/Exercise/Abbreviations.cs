80afccc7ec9eb50fad445dfd600fdec8dd77f49c
meikku <59999917+meikku@users.noreply.github.com>
2020-04-10 09:51:20 +0300

    ex. 139-140 done

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -1,23 +1,47 @@
 using System.Collections.Generic;
 
+
 namespace Exercise
 {
-  public class Abbreviations
-  {
-
-    public void AddAbbreviation(string abbreviation, string explanation)
+    public class Abbreviations // tämä on luokan nimi
     {
 
-    }
+        private Dictionary<string, string> abbreviations; // muuttujana kirjasto
+        public Abbreviations() // constructor
+        {
+            this.abbreviations = new Dictionary<string, string>(); // luodaan uusi kirjasto
 
-    public bool HasAbbreviation(string abbreviation)
-    {
-      return false;
-    }
+        }
+        public void AddAbbreviation(string abbreviation, string explanation)
+        {
+            
+            this.abbreviations.Add(abbreviation, explanation);
+          
 
-    public string FindExplanationFor(string abbreviation)
-    {
-      return null;
+            
+        }
+
+        public bool HasAbbreviation(string abbreviation)
+        {
+            if (this.abbreviations.ContainsKey(abbreviation))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string FindExplanationFor(string abbreviation)
+        {
+           
+
+           if (HasAbbreviation(abbreviation))
+           {  
+               return abbreviations[abbreviation];
+           }
+            else {
+                return "not found";
+            }
+            
+        }
     }
-  }
 }
