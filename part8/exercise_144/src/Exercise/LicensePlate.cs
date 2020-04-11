5c5b99b6102766653604e213dafd5016344d3583
meikku <59999917+meikku@users.noreply.github.com>
2020-04-11 17:25:12 +0300

    ex 144 done

--- part8/exercise_144/src/Exercise/LicensePlate.cs
+++ part8/exercise_144/src/Exercise/LicensePlate.cs
@@ -1,30 +1,50 @@
 namespace Exercise
 {
-  public class LicensePlate
-  {
-    public string liNumber { get; }
-    private string country;
-
-    public LicensePlate(string country, string liNumber)
+    public class LicensePlate
     {
-      this.liNumber = liNumber;
-      this.country = country;
-    }
+        public string liNumber { get; }
+        private string country;
 
+        public LicensePlate(string country, string liNumber)
+        {
+            this.liNumber = liNumber;
+            this.country = country;
+        }
 
-    public override string ToString()
-    {
-      return country + " " + liNumber;
-    }
+        public override string ToString()
+        {
+            return country + " " + liNumber;
+        }
 
-    public override bool Equals(object compared)
-    {
-      return false;
-    }
+        public override bool Equals(object compared)
+        {
+            if (this == compared)
+            {
+                return true;
+            }
 
-    public override int GetHashCode()
-    {
-      return -1;
+            // if the compared object is null or not of type Book, the objects are not equal
+            if ((compared == null) || !this.GetType().Equals(compared.GetType()))
+            {
+                return false;
+            }
+
+            else
+            {
+                LicensePlate comparedLicensePlate = (LicensePlate)compared;
+                return this.liNumber == comparedLicensePlate.liNumber && this.country == comparedLicensePlate.country;
+
+                // convert the object to a Book object
+
+                // if the values of the object variables are equal, the objects are, too
+
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            return this.liNumber.GetHashCode();
+        }
     }
-  }
 }
