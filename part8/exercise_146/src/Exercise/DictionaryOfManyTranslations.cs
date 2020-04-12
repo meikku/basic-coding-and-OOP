1c164873a94d107a0999c87b5ee914e1f99b640d
meikku <59999917+meikku@users.noreply.github.com>
2020-04-12 20:43:31 +0300

    ex. 146 done

--- part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
+++ part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
@@ -1,23 +1,46 @@
 using System.Collections.Generic;
+using System;
 
 namespace Exercise
 {
-  public class DictionaryOfManyTranslations
-  {
-
-    public void Add(string word, string translation)
+    public class DictionaryOfManyTranslations
     {
+        private Dictionary<string, List<string>> translations;
 
+        public DictionaryOfManyTranslations()
+        {
+            this.translations = new Dictionary<string, List<string>>();
+        }
+        public void Add(string word, string translation)
+        {
+
+            if (!this.translations.ContainsKey(word))
+            {
+                this.translations.Add(word, new List<string>());
+
+                translations[word].Add(translation);
+
+            }
+            else
+            {
+                this.translations[word].Add(translation);
+            }
+
+        }
+        public List<string> Translate(string word)
+        {
+
+            if (!translations.ContainsKey(word))
+            {
+                this.translations.Add(word, new List<string>());
+
+            }
+            return this.translations[word];
+        }
+
+        public void Remove(string word)
+        {
+            this.translations.Remove(word);
+        }
     }
-
-    public List<string> Translate(string word)
-    {
-      return new List<string>();
-    }
-
-    public void Remove(string word)
-    {
-
-    }
-  }
 }
