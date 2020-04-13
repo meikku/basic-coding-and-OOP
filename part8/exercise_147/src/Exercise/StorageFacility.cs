fb77868d6ea6f12bb0febeb5d9488f19b634b7af
meikku <59999917+meikku@users.noreply.github.com>
2020-04-13 21:24:29 +0300

    ex 147 done

--- part8/exercise_147/src/Exercise/StorageFacility.cs
+++ part8/exercise_147/src/Exercise/StorageFacility.cs
@@ -3,26 +3,53 @@ using System;
 
 namespace Exercise
 {
-  public class StorageFacility
-  {
-
-    public void Add(string unit, string item)
+    public class StorageFacility
     {
+        public Dictionary<string, List<string>> storedItems;
 
+        public StorageFacility()
+        {
+            this.storedItems = new Dictionary<string, List<string>>();
+        }
+        public void Add(string unit, string item)
+        {
+            if (!this.storedItems.ContainsKey(unit))
+            {
+                this.storedItems.Add(unit, new List<string>());
+            }
+
+            this.storedItems[unit].Add(item);
+        }
+
+        public List<string> Contents(string storageUnit)
+        {
+            if (!this.storedItems.ContainsKey(storageUnit))
+            {
+                this.storedItems.Add(storageUnit, new List<string>());
+            }
+            return this.storedItems[storageUnit];
+
+        }
+        public void Remove(string storageUnit, string item)
+        {
+            this.storedItems[storageUnit].Remove(item);
+            if (storedItems[storageUnit].Count == 0)
+            {
+                this.storedItems.Remove(storageUnit);
+            }
+
+        }
+
+        public List<string> StorageUnits()
+        {
+            List<string> printedItems = new List<string>();
+            Dictionary<string, List<string>>.KeyCollection keys = this.storedItems.Keys;
+            foreach (string printedUnits in keys)
+            {
+              printedItems.Add(printedUnits);
+            
+            }
+            return printedItems;
+        }
     }
-
-    public List<string> Contents(string storageUnit)
-    {
-      return new List<string>();
-    }
-
-    public void Remove(string storageUnit, string item)
-    {
-
-    }
-
-    public List<string> StorageUnits()
-    {
-      return new List<string>();
-    }
-  }
+}
