d24fb46ee6999f3d6fa749b24ed66529f3da4637
meikku <59999917+meikku@users.noreply.github.com>
2020-04-18 09:26:42 +0300

    ex. 150 done

--- part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
@@ -1,39 +1,76 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
-  public class ChangeHistory
-  {
-    private List<int> history;
-
-    public ChangeHistory()
+    using System;
+    using System.Collections.Generic;
+    public class ChangeHistory
     {
+        private List<int> history { get; set; }
+       
+       
 
+        public ChangeHistory()
+        {
+            this.history = new List<int>();
+           
+        }
+
+        public void Add(int status)
+        {
+            this.history.Add(status);
+        }
+
+        public void Clear()
+        {
+
+            this.history.Clear();
+        }
+
+        public int MaxValue()
+        {
+            int maxValue = 0;
+            foreach (int value in this.history)
+            {
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+
+            }
+            if (this.history.Count == 0)
+            {
+                return 0;
+            }
+            return maxValue;
+        }
+
+        public int MinValue()
+        {
+            int minValue = 0;
+            if (this.history.Count == 0)
+            {
+                return minValue;
+            }
+            else if (this.history.Count >= 1)
+            {
+                 minValue = this.history[0];
+           
+            foreach (int value in history)
+            {
+                if (value < minValue)
+                {
+                    minValue = value;
+                }
+
+            }
+            }
+            return minValue;
+        }
+
+        public override string ToString()
+        {
+            
+            int lastStatus = this.history.Count - 1;
+            return "Current: " + this.history[lastStatus] + " Min: " + MinValue() + " Max: " + MaxValue();
+        }
     }
-
-    public void Add(int status)
-    {
-
-    }
-
-    public void Clear()
-    {
-
-    }
-
-    public int MaxValue()
-    {
-      return 0;
-    }
-
-    public int MinValue()
-    {
-      return 0;
-    }
-
-    public override string ToString()
-    {
-      return "";
-    }
-  }
 }
--- part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
@@ -1,25 +1,44 @@
-namespace Exercise
+namespace Exercise 
 {
+using System.Collections.Generic;
+
   public class ProductWarehouseWithHistory : ProductWarehouse
+
   {
+    private int initialBalance { get; set; }
+    private ChangeHistory warehouseHistory { get; set; }
+    
+
     public ProductWarehouseWithHistory(string productName, int capacity, int initialBalance) : base(productName, capacity)
     {
+      this.productName = productName;
+      this.capacity = capacity;
+      this.initialBalance = initialBalance;
+      this.warehouseHistory = new ChangeHistory();
+      
+     
+     AddToWarehouse(initialBalance);
 
     }
 
     public string History()
     {
-      return "";
+      return this.warehouseHistory.ToString();
     }
 
     new public void AddToWarehouse(int amount)
     {
+      
+      base.AddToWarehouse(amount);
+      this.warehouseHistory.Add(this.balance);
 
+    
     }
-
     new public int TakeFromWarehouse(int amount)
     {
-      return 0;
+      base.TakeFromWarehouse(amount);
+      this.warehouseHistory.Add(this.balance);
+      return amount;
     }
   }
 }
