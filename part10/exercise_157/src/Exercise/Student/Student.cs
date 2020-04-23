bc317c33bd4f18a9043f522f053037f9468bed8b
meikku <59999917+meikku@users.noreply.github.com>
2020-04-23 13:13:28 +0300

    ex 157 done

--- part10/exercise_157/src/Exercise/Student/Student.cs
+++ part10/exercise_157/src/Exercise/Student/Student.cs
@@ -1,28 +1,33 @@
 namespace Exercise
 {
-  using System;
-  public class Student : IComparable<Student>
-  {
-
-    public string name { get; }
-
-    public Student(string name)
-    {
-      this.name = name;
-    }
-
-
-    public override string ToString()
-    {
-      return name;
-    }
-
-    //BEGIN SOLUTION
-    public int CompareTo(Student another)
+    using System;
+    public class Student : IComparable<Student>
     {
 
-      return 0;
+        public string name { get; }
+
+        public Student(string name)
+        {
+            this.name = name;
+        }
+
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        //BEGIN SOLUTION
+        public int CompareTo(Student another)
+        {
+            // -1 if this is earlier
+            // 0 if this is equal
+            // 1 if this is later
+            int result = this.name.CompareTo(another.name);
+            return result;
+
+            // or return this.name.CompareTo(another.name);
+        }
+        //END SOLUTION
     }
-    //END SOLUTION
-  }
 }
