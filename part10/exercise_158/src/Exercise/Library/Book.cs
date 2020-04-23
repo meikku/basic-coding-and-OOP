d376d23e01431a195a3d0cff074629ab26803884
meikku <59999917+meikku@users.noreply.github.com>
2020-04-23 14:01:26 +0300

    ex. 158

--- part10/exercise_158/src/Exercise/Library/Book.cs
+++ part10/exercise_158/src/Exercise/Library/Book.cs
@@ -1,31 +1,58 @@
 namespace Exercise
 {
-  using System;
-  public class Book : IComparable<Book>
-  {
-
-    public string name { get; set; }
-    public int age { get; set; }
-
-    public Book(string name, int age)
-    {
-      // Fill in the blanks
-    }
-
-
-    public override string ToString()
-    {
-      // Don't touch this
-      return this.name + " (recommended for " + this.age + " year-olds or older)";
-    }
-
-
-    public int CompareTo(Book other)
+    using System;
+    using System.Collections.Generic;
+    public class Book : IComparable<Book>
     {
 
-      // Do some magic here
-      return 0;
-    }
+        public string name { get; set; }
+        public int age { get; set; }
 
-  }
+        public Book(string name, int age)
+        {
+            this.name = name;
+            this.age = age;// Fill in the blanks
+        }
+
+
+        public override string ToString()
+        {
+
+            // Don't touch this
+            return this.name + " (recommended for " + this.age + " year-olds or older)";
+        }
+
+
+        public int CompareTo(Book other)
+        {
+
+            // Do some magic here
+            /*if (other == null)
+            {
+                return 1;
+            }
+            if (this.age == other.age)
+            {
+                return 0;
+            }
+            else if (this.age > other.age)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }*/
+            if (this.age == other.age)
+            {
+              return this.name.CompareTo(other.name);
+              
+
+            }
+            return (this.age.CompareTo(other.age));
+           
+
+        }
+
+    }
 }
--- part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
+++ part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
@@ -8,10 +8,31 @@ namespace Exercise
     private List<Book> books;
     public TextInterface()
     {
+      this.books = new List<Book>();
     }
 
     public void Start()
     {
+      while (true)
+      {
+      Console.WriteLine("Input the name of the book, empty stops:");
+      string name = Console.ReadLine();
+      if (name == "")
+      {
+        break;
+      }
+      else
+      {
+        Console.WriteLine("Input the age recommendation:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Book book = new Book(name, age);
+        this.books.Add(book);
+      }
+
+      
+      }
+      
+      this.books.ForEach(Console.WriteLine);
     }
   }
 }
