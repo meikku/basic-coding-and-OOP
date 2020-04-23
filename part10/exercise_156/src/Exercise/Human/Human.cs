0c814013ebd91672c921a6392a1271b2a53ac74d
meikku <59999917+meikku@users.noreply.github.com>
2020-04-23 12:47:03 +0300

    ex 156 done

--- part10/exercise_156/src/Exercise/Human/Human.cs
+++ part10/exercise_156/src/Exercise/Human/Human.cs
@@ -1,30 +1,46 @@
 namespace Exercise
 {
-  using System;
-  public class Human : IComparable<Human>
-  {
-
-    public int wage { get; }
-    public string name { get; }
-
-    public Human(string name, int wage)
+    using System;
+    public class Human : IComparable<Human>
     {
-      this.name = name;
-      this.wage = wage;
-    }
 
-    public int CompareTo(Human another)
-    {
-      // BEGIN SOLUTION
-      return 0;
-      // END SOLUTION
-    }
-    
+        public int wage { get; }
+        public string name { get; }
+
+        public Human(string name, int wage)
+        {
+            this.name = name;
+            this.wage = wage;
+        }
+
+        public int CompareTo(Human another)
+        {
+            // BEGIN SOLUTION
+            if (another == null)
+            {
+                return -1;
+            }
+            if (this.wage == another.wage)
+            {
+                return 0;
+            }
+            else if (this.wage > another.wage)
+            {
+              return -1;
+            }
+            else 
+            {
+              return 1;
+            }
+
+            // END SOLUTION
+        }
 
 
-    public override string ToString()
-    {
-      return name + " " + wage;
+
+        public override string ToString()
+        {
+            return name + " " + wage;
+        }
     }
-  }
 }
