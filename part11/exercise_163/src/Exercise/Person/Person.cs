284a606ada2cf0776d96632af84ad4eed936d885
meikku <59999917+meikku@users.noreply.github.com>
2020-04-29 10:28:20 +0300

    ex. 163 done

--- part11/exercise_163/src/Exercise/Person/Person.cs
+++ part11/exercise_163/src/Exercise/Person/Person.cs
@@ -11,6 +11,14 @@ namespace Exercise
     {
       this.name = name;
       this.age = age;
+    if (name == null || name == "" || name.Length > 40)
+    {
+      throw new ArgumentException();
+    }
+    if (age < 0 || age > 120)
+    {
+      throw new ArgumentException();
+    }
     }
   }
 }
