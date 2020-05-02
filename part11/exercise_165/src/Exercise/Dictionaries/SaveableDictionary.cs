3c7c9802e57c9c81395ef425f2acf9b38611e669
meikku <59999917+meikku@users.noreply.github.com>
2020-05-02 18:00:22 +0300

    ex 165 Save() not working yet

--- part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
+++ part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
@@ -1,39 +1,96 @@
 namespace Exercise
 {
-  using System;
-  using System.IO;
-  using System.Collections.Generic;
-  public class SaveableDictionary
-  {
-    public SaveableDictionary()
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    public class SaveableDictionary
     {
-    }
+        private Dictionary<string, string> dictionary;
+        private string file;
+        public SaveableDictionary()
+        {
+            this.dictionary = new Dictionary<string, string>();
+        }
 
-    public SaveableDictionary(string file) : this()
-    {
-    }
+        public SaveableDictionary(string file) : this()
+        {
+            this.file = file;
+        }
 
-    public void Add(string word, string translation)
-    {
-    }
+        public void Add(string word, string translation)
+        {
+            if (!this.dictionary.ContainsKey(word))
+                this.dictionary.Add(word, translation);
+            this.dictionary.Add(translation, word);
 
-    public bool Load()
-    {
-      return false;
-    }
+        }
 
-    public bool Save()
-    {
-      return false;
-    }
+        public bool Load()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(this.file);
 
-    public string Translate(string word)
-    {
-      return null;
-    }
 
-    public void Delete(string word)
-    {
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(":");
+                    string word = parts[0];
+                    string translation = parts[1];
+                    Add(word, translation);
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public bool Save()
+        {
+          
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(this.file);
+                foreach (KeyValuePair<string, string> kvp in this.dictionary)
+                {
+                    // if (!this.dictionary.ContainsKey(kvp.Value))
+                    // {
+                    sw.WriteLine(kvp.Key + ":" + kvp.Value);
+
+                    //}
+                }
+                sw.Close();
+                return true;
+
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+        public string Translate(string word)
+        {
+            if (!dictionary.ContainsKey(word))
+            {
+                return null;
+            }
+            return dictionary[word];
+        }
+
+        public void Delete(string word)
+        {
+            string translation = this.dictionary[word];
+            if (this.dictionary.ContainsKey(word))
+            {
+                this.dictionary.Remove(word);
+                this.dictionary.Remove(translation);
+            }
+        }
     }
-  }
 }
--- part11/exercise_165/src/Exercise/Program.cs
+++ part11/exercise_165/src/Exercise/Program.cs
@@ -1,12 +1,31 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
-  public class Program
-  {
-    public static void Main(string[] args)
+    using System;
+    using System.Collections.Generic;
+    public class Program
     {
+        public static void Main(string[] args)
+        {
 
+            SaveableDictionary dictionary = new SaveableDictionary("words.txt");
+            dictionary.Load();
+
+            // Translate all the words in the file both ways
+            Console.WriteLine(dictionary.Translate("apina"));
+            Console.WriteLine(dictionary.Translate("monkey"));
+            Console.WriteLine(dictionary.Translate("beer"));
+            Console.WriteLine(dictionary.Translate("olut"));
+            Console.WriteLine(dictionary.Translate("below"));
+            Console.WriteLine(dictionary.Translate("alla oleva"));
+
+            // Try adding, translating and removing a word, this should not affect the file
+            dictionary.Add("poista", "remove");
+            Console.WriteLine(dictionary.Translate("remove"));
+            dictionary.Delete("remove");
+
+            // Save the file
+            dictionary.Save();
+        }
     }
-  }
 }
+
